9a16a7160eefe3e4c9ca16e5cbfa8e480166edf9
ahmedgfouad <ahmedgfouad2020@gmail.com>
2025-08-05 15:08:44 +0300

    add firebase service

---
+++ lib/core/services/firebase_path.dart
@@ -0,0 +1,38 @@
+class FirestorePath {
+  static String planets() => "planets/";
+  static String products() => "products/";
+  static String categories() => "categories/";
+  static String user(String uid) => "users/$uid";
+
+  static String carts(String uid, String cartId) => "users/$uid/cards/$cartId";
+  static String myProductsCart(String uid) => "users/$uid/cards/";
+
+  static String favoriteProduct(String uid, String productId) =>
+      "users/$uid/favorite/$uid/products/$productId";
+  static String myFavoriteProduct(String uid) =>
+      "users/$uid/favorite/$uid/products/";
+
+  static String favoritePlanet(String uid, String planetId) =>
+      "users/$uid/favorite/$uid/planets/$planetId";
+  static String myFavoritePlanet(String uid) =>
+      "users/$uid/favorite/$uid/planets/";
+
+  static String garden(String uid, String planetId) =>
+      "users/$uid/garden/$planetId";
+  static String myGarden(String uid) => "users/$uid/garden/";
+
+  static String planetAlarm(String uid, String planetId,String alarmId) =>
+      "users/$uid/garden/$planetId/alarms/$alarmId";
+  static String myPlanetAlarm(String uid, String productId) =>
+      "users/$uid/garden/$productId/alarms/";
+
+  static String addresses(String uid, String addressId) =>
+      "users/$uid/addresses/$addressId";
+  static String myAddresses(String uid) => "users/$uid/addresses/";
+
+
+}
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sinna/core/router/app_router.dart';
 import 'package:sinna/core/theme/thems.dart';
+import 'package:sinna/firebase_options.dart';
 
 
-void main() {
+ Future <void> main() async{
+   WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
